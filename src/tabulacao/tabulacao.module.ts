c0d8da8d6047060b59b08a8eff5baa986e4dc0bc
Matheus Azevedo <matheusbzevedo@gmail.com>
2020-11-15 15:23:56 -0300

    importando mongoose em tabulacao

--- src/tabulacao/tabulacao.module.ts
+++ src/tabulacao/tabulacao.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TabulacaoSchema } from './interface/tabulacao.schema';
 import { TabulacaoController } from './tabulacao.controller';
 import { TabulacaoService } from './tabulacao.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Tabulacao', schema: TabulacaoSchema }])
+  ],
   controllers: [TabulacaoController],
   providers: [TabulacaoService]
 })
